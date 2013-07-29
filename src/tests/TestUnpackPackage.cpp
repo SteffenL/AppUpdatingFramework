712e4b5b2e70097cd3343b3ec2d1fa371c76155e
Steffen André Langnes <steffenl.sw@gmail.com>
2013-07-29 20:11:03 +0200

    Started improving tests

--- src/tests/TestUnpackPackage.cpp
+++ src/tests/TestUnpackPackage.cpp
@@ -5,21 +5,33 @@
 #include "core/package/sources.h"
 
 #include <UnitTest++.h>
+#include <direct.h>
 
 using namespace aufw;
 
 TEST(UnpackPackage) {
-    package::FilePackage package("package.zip");
-    package::SourceBase& source = package.GetSource();
+    _chdir("../../../bin/Debug/");
 
-    std::vector<std::string> fileList;
-    source.GetFileList(fileList);
+    std::vector<std::string> packageFileNames;
+    packageFileNames.push_back("package-7zip.zip");
+    packageFileNames.push_back("package-winrar.zip");
+    packageFileNames.push_back("package-winrar2.zip");
 
-    job::Job job;
-    // Backup files
-    job.AddStep(new job::MoveFilesStep(".", fileList, "update_backup"));
-    // Unpack
-    job.AddStep(new job::UnpackFilePackageStep(package, "."));
-    job.Execute();
-    job.Rollback();
+    for (auto& fileName : packageFileNames) {
+        std::string filePath("testdata/packages/");
+        filePath += fileName;
+        package::FilePackage package(filePath);
+        package::SourceBase& source = package.GetSource();
+
+        std::vector<std::string> fileList;
+        source.GetFileList(fileList);
+
+        job::Job job;
+        // Backup files
+        job.AddStep(new job::MoveFilesStep(".", fileList, "update_backup"));
+        // Unpack
+        job.AddStep(new job::UnpackFilePackageStep(&package, "."));
+        job.Execute();
+        job.Rollback();
+    }
 }
--- src/tests/main.cpp
+++ src/tests/main.cpp
@@ -1,5 +1,11 @@
 #include <UnitTest++.h>
+#include <boost/locale.hpp>
+#include <boost/filesystem/path.hpp>
 
 int main() {
+    // Setup locale
+    std::locale::global(boost::locale::generator().generate(""));
+    boost::filesystem::path::imbue(std::locale());
+
     return UnitTest::RunAllTests();
 }
